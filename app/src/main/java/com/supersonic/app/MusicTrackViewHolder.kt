3197a262bad90b30b1e9700884bea3fca2e67093
lalit <lalit.appsmail@gmail.com>
2019-08-14 00:12:44 +0530

    initial commit

---
+++ app/src/main/java/com/supersonic/app/MusicTrackViewHolder.kt
@@ -0,0 +1,32 @@
+package com.supersonic.app
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+class MusicTrackViewHolder(itemView: View) : ViewHolder(itemView) {
+
+    private val imgThumb: ImageView = itemView.findViewById(R.id.img_thumb)
+    private val txtMusicTitle: TextView = itemView.findViewById(R.id.txt_music_title)
+    private val txtMusicAuthor: TextView = itemView.findViewById(R.id.txt_music_author)
+
+    override fun onBindView(model: Any) {
+        val musicFile = model as MusicTrackDetails
+
+        if (musicFile.musicFileTitle.isNotBlank()) {
+            txtMusicTitle.text = musicFile.musicFileTitle
+        }
+
+        if (musicFile.musicFileArtist.isNotBlank()) {
+            txtMusicAuthor.text = musicFile.musicFileArtist
+        }
+
+        if (musicFile.musicFileThumb.isNotBlank()) {
+            Glide.with(itemView.context)
+                .load(musicFile.musicFileThumb)
+                .into(imgThumb)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/supersonic/app/ViewHolder.kt
@@ -0,0 +1,12 @@
+package com.supersonic.app
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    abstract fun onBindView(model : Any)
+
+}
